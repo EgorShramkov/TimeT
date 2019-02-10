69dfa7a50e3bcda1f612f8906f9e42402d787325
EgorShramkov <45947725+EgorShramkov@users.noreply.github.com>
2019-02-10 13:33:39 +0300

    Update serverTT.py

--- timeT/serverTT.py
+++ timeT/serverTT.py
@@ -94,7 +94,7 @@ class MainHandler(tornado.web.RequestHandler):
         r = redis.from_url(os.environ.get("REDIS_URL"))
         self.render(pages["main_page"], message="Доброго времени суток")
         r.flushdb()
-        r.set('1_5_1', '2')
+        r.set('1_5_1', 2)
         r.set('1_5_2', '3')
         r.set('1_5_3', '1')
         r.set('1_5_4', '1')
