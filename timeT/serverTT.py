0c2f6e53da58afad51eeb2a8d2f43cc43835b94b
EgorShramkov <45947725+EgorShramkov@users.noreply.github.com>
2019-02-10 14:48:46 +0300

    Update serverTT.py

--- timeT/serverTT.py
+++ timeT/serverTT.py
@@ -92,7 +92,6 @@ def save_send(bot, self,NewLesson,Lesson, NOM, chat_id_mass, day, allLesson, cha
 class MainHandler(tornado.web.RequestHandler):
      def get(self):
         r = redis.from_url(os.environ.get("REDIS_URL"))
-        self.render(pages["main_page"], message="Доброго времени суток")
         r.set('1_5_1',3)
         r.set('1_5_2',3)
         r.set('1_5_3',1)
@@ -443,6 +442,8 @@ class MainHandler(tornado.web.RequestHandler):
         r.set('5_11_8', 1)
         r.set('5_11_9', 1)
         r.set('5_11_10', 1)
+        otvet = r.get('1_5_1',3) + r.get('1_5_2',3) + r.get('1_5_3',1)
+         self.render(pages["main_page"], message=otvet)
 
 class Page_allday_saveHandler(tornado.web.RequestHandler):
      def post(self):
